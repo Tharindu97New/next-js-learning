03ae4656f6cf50e9f51ab9a4745e002d75ca3a7b
Tharindu Kavishna <156530474+Tharindu97New@users.noreply.github.com>
2024-02-28 19:23:31 +0530

    Add Local & Remote Images | Image Optimizations & Best practices

--- first-next-app/app/page.jsx
+++ first-next-app/app/page.jsx
@@ -1,13 +1,28 @@
 import Link from "next/link";
 import styles from "./page.module.css";
+import Image from "next/image";
+import img1 from '../public/1.jpg';
 
 export default function Home() {
   return (
     <>
-      <div>
+      <h1>Image Component</h1>
+      <Image 
+        src={img1}
+        alt="Sample img"
+        // style={{width: "100%", objectFit: "cover"}}
+        className="imgStyle"
+      />
+    </>
+  );
+}
+
+
+      {/* <div>
         <h1>Home Page</h1>
-        <div>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available </div>
-      </div>
+        <div>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available</div>
+      </div> */}
+      
       {/* <ul>
         <li><Link href="/" className={styles.link} >Home</Link></li>
         <li><Link href="/services" className={styles.link}>Services</Link></li>
@@ -18,6 +33,3 @@ export default function Home() {
         <li><Link href="/products/meat" className={styles.link}>Meat Page</Link></li>
         <li><Link href="/products/vegis" id={styles.specalLink}>Vegis Page</Link></li>
       </ul> */}
-    </>
-  );
-}
--- first-next-app/app/user/layout.js
+++ first-next-app/app/user/layout.js
@@ -1,6 +1,6 @@
 export default function ProfileLayout({ children }) {
   return (
-    <div className="wrapper">
+    <div>
       <div className="row">
         <h3>Profile Header</h3>
       </div>
